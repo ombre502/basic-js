4f5fc5182482b4a8932ad8e7cf2b37999499c913
ombre502 <ombre502@gmail.com>
2022-05-02 20:33:59 +0200

    feat: hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,20 +3,30 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function calculateHanoi( disksNumber, turnsSpeed ) {
+let k = 3600/turnsSpeed;
+let countTurns=0;
+let arr=[0,1,3];
+let returnObject = { turns: 0, seconds: 0 };
+
+  for (let i=2; i<disksNumber;i++){
+    countTurns+=Math.pow(2, i);
+  }
+
+  if ( disksNumber < 3 ) return { turns: arr[disksNumber], seconds: Math.floor(arr[disksNumber]*k) } ;
+  return  { turns:countTurns+3, seconds: Math.floor( (countTurns+3)*k )};
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -2,19 +2,38 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+const NOARGUMENT = "Unable to determine the time of year!";
+const INVALIDDATE = 'Invalid date!';
+
+const SEASONS = { [11]:"winter",[0]:"winter",[1]:"winter", [2]:"spring",[3]:"spring",[4]:"spring",
+  [5]:"summer",[6]:"summer",[7]:"summer",[8]:"autumn",[9]:"autumn",[10]:"autumn"};
+
+
+function getSeason(date ) {
+
+
+  if (arguments.length<1) return NOARGUMENT;
+
+
+  if (typeof date !== 'object' ) { return new Error( INVALIDDATE);}
+  if ( date == null ) { return new Error( INVALIDDATE);}
+  if (!date && isNaN(date.getTime()) ) { return new Error( INVALIDDATE);}
+
+
+  return SEASONS[date.getMonth()];
 }
+
+console.log(getSeason(new Date(2022, 1, 29)));
 
 module.exports = {
   getSeason
